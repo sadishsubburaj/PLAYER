934de0d7f2d8be06139d39cb8378e473ead5e939
sadishsubburaj <sadishsubbu@gmail.com>
2017-06-05 17:24:35 +0530

    Update Positive.java

--- Positive.java
+++ Positive.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Positive
 { 
-   public static void main()
+   public static void main(String args[])
    {
       Scanner p=new Scanner(System.in);
       int number;
