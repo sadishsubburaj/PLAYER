230ec4ce58a3cfad9b59f407b4e089adc301b548
sadishsubburaj <sadishsubbu@gmail.com>
2017-06-06 13:44:07 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -4,12 +4,19 @@ public class Factorial
  public static void main(String args[])
  {  
     Scanner p=new Scanner(System.in)
-    int a,i,b=0;
+    int a,i,b=1;
     a=p.nextInt();
+  if((a>0)&&(a<100))
+  {
     for(i=1;i<=a;i++)
     {
       b=b*i;
     }
     System.out.print(b);
- }
+  }
+  else
+  {
+   System.out.print("NOT IN RANGE");
+  }
+  }
 }
