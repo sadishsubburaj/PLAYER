574fc956439ed2b07d70acdf3de9264c90e20cea
sadishsubburaj <sadishsubbu@gmail.com>
2017-06-08 11:36:48 +0530

    Create Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -3,20 +3,14 @@ public class Factorial
 {
  public static void main(String args[])
  {  
-    Scanner p=new Scanner(System.in)
-    int a,i,b=1;
-    a=p.nextInt();
-  if((a>0)&&(a<100))
-  {
-    for(i=1;i<=a;i++)
-    {
-      b=b*i;
-    }
+    Scanner p=new Scanner(System.in);
+    String a=new String();
+    a=p.next();
+    StringBuffer b=new StringBuffer(a);
+    b.reverse();
     System.out.print(b);
-  }
-  else
-  {
-   System.out.print("NOT IN RANGE");
-  }
+    
+    
+   
   }
 }
