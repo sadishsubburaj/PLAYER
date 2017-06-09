390c492a717f92b468f6fa8c8deaf8c4bc0f81b6
sadishsubburaj <sadishsubbu@gmail.com>
2017-06-09 15:52:27 +0530

    Create Least.java

---
+++ Least.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class Least
+{
+  public static void main(String args[])
+  {
+      Scanner p=new Scanner(System.in);
+      int n,k,i,j;
+      n=p.nextInt();
+      k=p.nextInt();
+      int a[]=new int[100];
+      for(i=0;n>0;i++)
+      {
+         a[i]=n%10;
+         n=n/10;
+       }  
+       Arrays.sort(a,0,i);
+       for(j=0;j<i-k;j++)
+        System.out.print(a[j]);
+  }
+  
+}
