8e08fb627512ee0470d0768269e2dda6f6cd69b9
sadishsubburaj <sadishsubbu@gmail.com>
2017-06-12 08:11:14 +0530

    Create Largest.java

--- Largest.java
+++ Largest.java
@@ -3,15 +3,20 @@ class Largest
 {
    public static void main(String args[])
    { 
-      int n;
-      Scanner p.new Scanner(System.in);
-  
+      int n,i;
+      Scanner p=new Scanner(System.in);
+      
       n=p.nextInt();
+      if(n<10)
+      {
       String[] s1=new String[n];
       for(i=0;i<n;i++)
       s1[i]=p.next();
       Arrays.sort(s1);
       for(i=0;i<n;i++)
       System.out.print(s1[i]);
+      }
+      else
+         System.out.print("NOT IN RANGE");
    }
 }
