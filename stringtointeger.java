1d4ed213129abf94b10f54864524388aa08cc6e5
sadishsubburaj <sadishsubbu@gmail.com>
2017-10-05 19:35:05 +0530

    Create stringtointeger.java

---
+++ stringtointeger.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class stringtointeger
+{
+public static void main(String ars[])
+{
+  Scanner p=new Scanner(System.in);
+  String s1=p.next();
+  int n1=Integer.parseInt(s1);
+  System.out.print(n1);
+}
+}
